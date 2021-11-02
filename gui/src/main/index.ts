4f65ecfdebc1e5c915960f2c489130c06da97f42
Oskar Nyberg <oskar@mullvad.net>
2021-11-02 23:55:23 +0100

    Replace use of setAccount with new calls

--- gui/src/main/index.ts
+++ gui/src/main/index.ts
@@ -1341,7 +1341,7 @@ class ApplicationMain {
         log.warn(`Failed to get account data, logging in anyway: ${verification.error.message}`);
       }
 
-      await this.daemonRpc.setAccount(accountToken);
+      await this.daemonRpc.loginAccount(accountToken);
     } catch (e) {
       const error = e as Error;
       log.error(`Failed to login: ${error.message}`);
@@ -1380,7 +1380,7 @@ class ApplicationMain {
 
   private async logout(): Promise<void> {
     try {
-      await this.daemonRpc.setAccount();
+      await this.daemonRpc.logoutAccount();
 
       this.accountExpiryNotificationScheduler.cancel();
     } catch (e) {
